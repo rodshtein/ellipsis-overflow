aa68b657ad496a4caf9e7c6b7d076db5b3b8cbc5
Misha Zamkevich <uwwcom@gmail.com>
2021-01-03 20:57:22 +1000

    Ready 🎉🎊🥳

--- src/ellipsis.js
+++ src/ellipsis.js
@@ -18,182 +18,163 @@ export function ellipsis(node, {
   cutLength = 0,
   overflowBadge = '…'
 }={} ) {
-  let text = '';
-  let firstInit = true;
-  let viewportWidth;
-  let viewportHeight;
+  let paragraphRuler, clone;
+  let sizeRuler = document.createElement('span');
+  let sliceCycle = 0;
+  let resizeObserverBlankRun = true;
 
-  // Make Shadow
-  let shadowParagraph = node.cloneNode(true);
-  document.body.append(shadowParagraph)
-
-  function getParagraphMargins(){
-    let margin = 'margin:' + getComputedStyle(node).margin + ';';
-    let padding = 'padding' + getComputedStyle(node).padding + ';';
-    return margin + padding;
-  }
-
-  // create new paragraph
-  let ellipsisParagraph = node.cloneNode(true);
-  ellipsisParagraph.setAttribute('ariaHidden', true)
-  ellipsisParagraph.style.position = 'unset';
-  node.before(ellipsisParagraph)
+  // Prepare parent node
   node.parentNode.style.position = 'relative';
-  node.style.cssText = visibilityHidden
 
-  // create and set tester node
-  let sizeHolder = document.createElement('span');
+  // Observers
+  let resizeObserver = new ResizeObserver(resizeReCalc)
+  let mutationObserver = new MutationObserver(mutationReCalc)
 
-  // Height tester
-  function measureSize() {
-    text = node.textContent
-    ellipsisParagraph.append(sizeHolder)
-    sizeHolder.style.cssText = fillStyle + getParagraphMargins()
-    viewportWidth = sizeHolder.clientWidth
-    viewportHeight = sizeHolder.clientHeight
-    shadowParagraph.style.width = viewportWidth + 'px'
-    calc()
+  // init node container
+  let nodesContainer = window.__ellipsisNodeContainer;
+  if(!nodesContainer){
+    nodesContainer = document.createElement('div')
+    nodesContainer.setAttribute('ariaHidden', true)
+    nodesContainer.setAttribute('id', 'ellipsisNodeContainer')
+    nodesContainer.style.cssText = visibilityHidden
+    document.body.append(nodesContainer)
+    window.__ellipsisNodeContainer = nodesContainer
   }
 
-  function calc(){
-    let overflowRatio = viewportHeight / ( node.clientHeight / 100);
-    shadowParagraph.innerText = text
-    if(shadowParagraph.clientHeight <= viewportHeight && firstInit ){
-      firstInit = false
-      // revert
-      // event
+  // Create ellipsis paragraph
+  function cloneNode(){
+    let _clone = node.cloneNode(true);
+    _clone.style.position = 'unset';
+
+    if(typeof clone == 'object') {
+      clone.before(_clone)
+      clone.remove();
+      clone = _clone;
     } else {
-      sliceString(Math.round(text.length / 100 * overflowRatio))
+      clone = _clone;
+      node.before(clone)
+      nodesContainer.append(node)
     }
   }
+  cloneNode()
 
-  function sliceString(length, prevOverflow) {
-    let string = text.substr(0, length) + overflowBadge;
+
+  // Set Size Ruler
+  function setSizeRuler(){
+    sizeRuler.style.cssText = fillStyle + getParagraphMargins(node)
+    clone.append(sizeRuler)
+  }
+  setSizeRuler()
+
+  // Set height calc paragraph
+  function setParagraphRuler(){
+    if(typeof paragraphRuler == 'object') paragraphRuler.remove()
+    paragraphRuler = node.cloneNode(true);
+    nodesContainer.append(paragraphRuler)
+  }
+  setParagraphRuler()
+
+
+  // Calc init
+  function calc(){
+    // We subtract one pixel because chrome
+    // always round client* sizes upwards
+    // but for paragraph height calc it use real sizes
+    paragraphRuler.style.width = sizeRuler.clientWidth - 1 + 'px'
+
+    // We always reset text to measure the height difference
+    paragraphRuler.textContent = node.textContent
+
+    if(sizeRuler.clientHeight < paragraphRuler.clientHeight) {
+      let ratio = sizeRuler.clientHeight / ( clone.clientHeight / 100);
+      sliceString(Math.round(clone.textContent.length / 100 * ratio))
+    }
+  }
+  calc()
+
+
+  function sliceString(length, isOverflow, currCycle = false) {
+    currCycle = currCycle ? currCycle : ++sliceCycle;
+
+    let string = node.textContent.substr(0, length) + overflowBadge;
     let textHeight;
 
-    shadowParagraph.innerText = string
-    textHeight = shadowParagraph.clientHeight
+    // kill slicer if we have new cycle
+    if(currCycle != sliceCycle)  return
+
+    paragraphRuler.textContent = string
+    textHeight = paragraphRuler.clientHeight
 
     // for string decrease
-    if( textHeight > viewportHeight ){
-      if( prevOverflow == false ){
+    if( textHeight > sizeRuler.clientHeight ){
+      if( isOverflow == false ){
         addShortText(--length)
       } else {
-        sliceString(--length, true)
+        sliceString(--length, true, currCycle)
       }
     }
 
     // for string increase
-    if( textHeight <= viewportHeight ){
-      if( prevOverflow == true ) {
+    if( textHeight <= sizeRuler.clientHeight ){
+      if( isOverflow == true ) {
         addShortText(length)
       } else {
-        sliceString(++length, false)
+        sliceString(++length, false, currCycle)
       }
     }
   }
 
   function addShortText(length){
-    ellipsisParagraph.textContent = text.substr(0, length - cutLength) + overflowBadge;
+    clone.textContent = node.textContent.substr(0, length - cutLength) + overflowBadge;
+    setSizeRuler()
+    setResizeObserver(sizeRuler)
   }
 
-  // resizeObserver(node)
-  let isThrottle = false;
-  let queue = false;
+  function mutationReCalc(){
+    cloneNode()
+    setSizeRuler()
+    setParagraphRuler()
+    calc()
+  }
 
-  let ro = new ResizeObserver(() => {
-    if (!isThrottle){
-      measureSize()
-      isThrottle = true
 
-      setTimeout(()=>{
-        if (queue) {
-          measureSize()
-          console.log('ЗАЕБАЛА!!!')
-          queue = false
-        }
-        isThrottle = false
-      }, 300)
+  function setResizeObserver(target){
+    resizeObserverBlankRun = true;
+    resizeObserver.observe(target);
+  }
+
+
+  // Resize Observer
+  let resizeDebounce = false;
+  function resizeReCalc(){
+    // Prevent blank run that initiated by an observer
+    if(resizeObserverBlankRun){
+      resizeObserverBlankRun = false;
     } else {
-      queue = true
+      if (resizeDebounce == false){
+        resizeDebounce = true
+        setTimeout(()=>{
+          calc()
+          resizeDebounce = false
+        }, 300)
+      }
     }
-  })
+  }
 
-  let mo = new MutationObserver(measureSize)
 
-  // measureSize()
-  ro.observe(ellipsisParagraph);
-  mo.observe(node, {
+  mutationObserver.observe(node, {
     attributes: true,
     characterData: true,
     subtree: true,
   });
 
   return {
-    destroy() {
-      // 	node.removeEventListener('touchstart', onDown);
-    }
+    destroy() { }
   };
 }
 
-
-function throttle(func, ms) {
-
-  let isThrottled = false,
-    savedArgs,
-    savedThis;
-
-  function wrapper() {
-
-    if (isThrottled) {
-      // запоминаем последние аргументы для вызова после задержки
-      savedArgs = arguments;
-      savedThis = this;
-      return;
-    }
-
-    // в противном случае переходим в состояние задержки
-    func.apply(this, arguments);
-
-    isThrottled = true;
-
-    // настройка сброса isThrottled после задержки
-    setTimeout(function() {
-      isThrottled = false;
-      if (savedArgs) {
-        // если были вызовы, savedThis/savedArgs хранят последний из них
-        // рекурсивный вызов запускает функцию и снова устанавливает время задержки
-        wrapper.apply(savedThis, savedArgs);
-        savedArgs = savedThis = null;
-      }
-    }, ms);
-  }
-
-  return wrapper;
-}
-
-function resizeObserver(node, handler){
-  let frame = document.createElement('iframe');
-  frame.style.cssText = `
-    position:absolute;
-    left:0;
-    top:-100%;
-    width:100%;
-    height:100%;
-    margin:1px 0 0;
-    border:none;
-    opacity:0;
-    visibility:hidden;
-    pointer-events:none;`;
-  frame.setAttribute('aria-hidden', true)
-  frame.setAttribute('tabindex', '-1')
-  frame.setAttribute('src', 'about:blank')
-  node.appendChild(frame)
-
-  console.log('height: ' + frame.clientHeight)
-
-  frame.contentWindow.onresize = () => {
-    console.log('height: ' + frame.clientHeight)
-    // 		handler.call(node.parentNode)
-  };
+function getParagraphMargins(paragraph){
+  let margin = 'margin:' + getComputedStyle(paragraph).margin + ';';
+  let padding = 'padding' + getComputedStyle(paragraph).padding + ';';
+  return margin + padding;
 }
